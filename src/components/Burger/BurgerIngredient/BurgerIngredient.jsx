e935b84f464a9457060a9337282a59a261234084
Viki Zhang <onlyzj@gmail.com>
2018-08-18 01:05:13 +0800

    finish BurgerIngredient.jsx and Burgredient.css of step 115

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import classes from "./BurgerIngredient.css";
+import PropTypes from "prop-types";
+
+// const burgerIngredient = () => {
+
+// export default burgerIngredient;
+
+class BurgerIngredient extends Component {
+  render() {
+    return;
+    let ingredient = null;
+    switch (this.props.type) {
+      case "bread-bottom":
+        ingredient = <div className={classes.BreadBottom} />;
+        break;
+      case "bread-top":
+        ingredient = (
+          <div className={classes.BreadTop}>
+            <div className="classes.Seeds1" />
+            <div className="classes.Seeds2" />
+          </div>
+        );
+        break;
+      case "meat":
+        ingredient = <div className={classes.Meat} />;
+        break;
+      case "cheese":
+        ingredient = <div className={classes.Cheese} />;
+        break;
+      case "Salad":
+        ingredient = <div className={classes.Salad} />;
+        break;
+      case "bacon":
+        ingredient = <div className={classes.Cheese} />;
+        break;
+
+      default:
+        ingredient = null;
+        break;
+    }
+    return ingredient;
+  }
+}
+
+BurgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired
+};
+
+export default BurgerIngredient;
