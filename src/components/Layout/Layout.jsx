a5d7c6a4fb8503764a1585da180c12264034cb9f
Viki Zhang <onlyzj@gmail.com>
2018-08-18 00:29:23 +0800

    confirm css module works.

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import Aux from "../../hoc/Aux";
+// import Aux from "../../hoc/Aux";
+import classes from "./Layout.css";
 
 const layout = props => (
-  <Aux>
+  <React.Fragment>
     <div>Toolbar, SideDrawer, BackDrop</div>
-    <main>{props.children}</main>
-  </Aux>
+    <main className={classes.content}>{props.children}</main>
+  </React.Fragment>
 );
 
 export default layout;
