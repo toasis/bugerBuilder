e6582c271e7e6dc976937f68f07eecfa32f2f2e5
Viki Zhang <onlyzj@gmail.com>
2018-08-18 23:00:51 +0800

    render basic Navigation component successfully.

--- src/components/Navigation/Toolbar/Toolbar.jsx
+++ src/components/Navigation/Toolbar/Toolbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import classes from "./Toolbar.css";
 import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 const toolBar = props => {
   return (
     <header className={classes.Toolbar}>
       <div>MENU</div>
       <Logo />
-      <nav>...</nav>
+      <NavigationItems />
     </header>
   );
 };
