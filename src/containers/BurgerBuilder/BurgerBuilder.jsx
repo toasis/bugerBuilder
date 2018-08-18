eb1027f9cb04324cd5bccd2a5a09c154366915c5
Viki Zhang <onlyzj@gmail.com>
2018-08-18 10:48:20 +0800

    change burger.js to jsx format

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Burger from "../../components/Burger/Burger.js";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
   state = {};
