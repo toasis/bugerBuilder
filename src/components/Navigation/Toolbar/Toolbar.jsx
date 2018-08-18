cf161cdd1b41728a3a252e80f9c6821111fac50a
Viki Zhang <onlyzj@gmail.com>
2018-08-18 22:14:34 +0800

    render Logo component in Toolbar

--- src/components/Navigation/Toolbar/Toolbar.jsx
+++ src/components/Navigation/Toolbar/Toolbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import classes from "./Toolbar.css";
+import Logo from "../../Logo/Logo";
 const toolBar = props => {
   return (
     <header className={classes.Toolbar}>
       <div>MENU</div>
-      <div>Logo</div>
+      <Logo />
       <nav>...</nav>
     </header>
   );
