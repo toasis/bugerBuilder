f213b9f85da703a6c990789f0a832c6911dc4df4
Viki Zhang <onlyzj@gmail.com>
2018-08-19 13:01:47 +0800

    The BackDrop now works in mobile view.

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 // import Aux from "../../hoc/Aux";
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import { timingSafeEqual } from "crypto";
 
-const layout = props => (
-  <React.Fragment>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.content}>{props.children}</main>
-  </React.Fragment>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  };
 
-export default layout;
+  sideDrawerCloseHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Toolbar />
+        <SideDrawer
+          closed={this.sideDrawerCloseHandler}
+          open={this.state.showSideDrawer}
+        />
+        <main className={classes.content}>{this.props.children}</main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Layout;
