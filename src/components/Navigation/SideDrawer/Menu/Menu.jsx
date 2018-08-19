a438355a7c9c77f1a4e012132c03b9ac7a2fb53e
Viki Zhang <onlyzj@gmail.com>
2018-08-19 16:01:05 +0800

    change the menu to hanberger menu of mobile screen

--- src/components/Navigation/SideDrawer/Menu/Menu.jsx
+++ src/components/Navigation/SideDrawer/Menu/Menu.jsx
@@ -3,7 +3,9 @@ import classes from "./Menu.css";
 const Menu = props => {
   return (
     <div className={classes.Menu} onClick={props.toggleSideDrawer}>
-      MENU
+      <div />
+      <div />
+      <div />
     </div>
   );
 };
